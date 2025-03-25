6ec8b87332112a97cf4b3fd8941dd43a0916a037
Emmanuel Ilustrisimo <ilustrisimo.emmanuell@gmail.com>
2025-03-25 03:05:14 -1200

    Adding all fadein

--- PC-REX-ECOMMERCE/src/JS/login_signupPage.js
+++ PC-REX-ECOMMERCE/src/JS/login_signupPage.js
@@ -19,3 +19,25 @@ function togglePasswordVisibility(inputId, iconElement) {
 }
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll("a, div"); // Kasama na ang navbar
+
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("opacity-100");
+                    entry.target.classList.remove("opacity-0");
+                } else {
+                    entry.target.classList.add("opacity-0"); // Magiging invisible ulit kapag lumabas sa viewport
+                }
+            });
+        },
+        { threshold: 0.2 }
+    );
+
+    elements.forEach((element) => observer.observe(element));
+});
